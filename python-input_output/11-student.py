6ccea52e2c5b1202c8a2e9ebc02c51a01bb75cbe
takurandoro <t.ndoro1@alustudent.com>
2023-03-23 14:37:33 -0700

    f

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
-"""
-Implements a square
-"""
-Rectangle = __import__('9-rectangle').Rectangle
+"""class_to_json"""
 
 
-class Square(Rectangle):
+class Student:
+    """Contains student data
     """
-    class Square - represents a square
-    methods:
-        - area() - returns the area
-    """
-    def __init__(self, size):
-        super().integer_validator('size', size)
-        self.__size = size
 
-    def area(self):
-        return self.__size ** 2
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-    def __str__(self):
-        return f'[Square] {self.__size}/{self.__size}'
+    def to_json(self, attrs=None):
+        """class_to_json"""
+
+        if attrs is None or type(attrs) != list:
+            return self.__dict__
+        else:
+            temp = {}
+            for elem in attrs:
+                if type(elem) != str:
+                    return self.__dict__
+                if elem in self.__dict__.keys():
+                    temp[elem] = self.__dict__[elem]
+            return temp
+
+    def reload_from_json(self, json):
+        """reload_from_json"""
+
+        for items in json.keys():
+            self.__dict__[items] = json[items]
